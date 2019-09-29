3ea19be41e60ab84268416afd173072bf6af05a2
sspog <sspog.lim@gmail.com>
2019-09-29 23:22:12 +0900

    Add Makefile(by CMake), Modify commend

---
+++ CMakeFiles/TestCMake.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestCMake.dir/main.cpp.o"
+  "CMakeFiles/TestCMake.dir/src/display.cpp.o"
+  "CMakeFiles/TestCMake.dir/src/person.cpp.o"
+  "CMakeFiles/TestCMake.dir/src/student.cpp.o"
+  "CMakeFiles/TestCMake.dir/src/student_list_manager.cpp.o"
+  "CMakeFiles/TestCMake.dir/src/utils.cpp.o"
+  "TestCMake"
+  "TestCMake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestCMake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -5,19 +5,24 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     string dataFileName;
-    if (argc == 1) {
-        cout << "Create a new file <data.txt> if one does not already exist.\n";
-        dataFileName = "data.txt";
-    } else if (argc == 2) {
-        cout << "Create a new file <" << argv[1] << ">";
-        dataFileName = argv[1];
+    if (argc <= 2) {
+
+        if (argc == 1) {
+            cout << "Create a new file <data.txt> if one does not already exist.\n";
+            dataFileName = "data.txt";
+        } else { // argc == 2
+            cout << "Create a new file <" << argv[1] << ">";
+            dataFileName = argv[1];
+        }
+
+        StudentListManager *stu = new StudentListManager(dataFileName);
+        Display display;
+        display.mainView(*stu);
+        delete(stu);
+
     } else { // argc > 2
         cout << "Wrong option - please choose only one .txt file";
     }
 
-    StudentListManager *stu = new StudentListManager(dataFileName);
-    Display display;
-    display.mainView(*stu);
-    delete(stu);
     return 0;
 }
