dabe85ca30a0b3798c329a0767e0a6fd4b35d209
sspog <sspog.lim@gmail.com>
2019-09-29 22:18:47 +0900

    Modify terminal commends' message (argc, argv)

--- main.cpp
+++ main.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     string dataFileName;
-    if (argc != 1){
-        dataFileName = argv[1];
-    }
-    else {
-        cout << "Create a new file <data.txt>\n";
+    if (argc == 1) {
+        cout << "Create a new file <data.txt> if one does not already exist.\n";
         dataFileName = "data.txt";
+    } else if (argc == 2) {
+        cout << "Create a new file <" << argv[1] << ">";
+        dataFileName = argv[1];
+    } else { // argc > 2
+        cout << "Wrong option - please choose only one .txt file";
     }
 
     StudentListManager *stu = new StudentListManager(dataFileName);
