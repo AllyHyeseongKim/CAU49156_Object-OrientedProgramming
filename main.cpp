c26b8f15c27aba2169ff4087d297b74afff08040
Jang Wonbeom <jtiger958@gmail.com>
2019-09-29 12:37:43 +0900

    reset main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "headers/student_list_manager.h"
 #include "headers/search.h"
+#include  "headers/display.h"
 
 using namespace std;
 
@@ -9,23 +10,72 @@ void printStudentList(vector<Student> list){
     cout << "LIST" << endl;
     cout << "    ID     |       NAME      |  AGE  |      DEPARTMENT      |       TEL      |" << endl;
     for (Student student: list) {
-        cout << setw(10) << student.getId() << " | " << setw(15) << student.getName() << " |  " << setw(4)
-             << student.getAge()
-             << " | " << setw(20) << student.getDepartment() << " | " << setw(14) << student.getTel() << " | "
-             << endl;
+        cout << setw(10) << student.getId() << " | "        // "ID" has up to 10
+            << setw(15) << student.getName() << " |  "      // "Name" has up to 15
+            << setw(4) << student.getAge() << " | "         // "Age" has up to 3
+            << setw(20) << student.getDepartment() << " | " // "Department" has up to 20
+            << setw(14) << student.getTel() << " | "        // "Tel" has up to 12
+            << endl;
     }
 }
+
+void viewPagerStudentList(vector<Student> list) {
+    // start from 0 page
+    int input = 1, i = -1;
+    while(true){
+        if(input == 1) {
+            if((i+1)*5 >= list.size()) {
+                cout << "- End of Page - " << "\n\n";
+            }
+            else {
+                i++;
+                printStudentList(vector<Student>(list.begin() + i*5, list.begin() + i*5+5));
+            }
+        } else if(input == 2) {
+            if(i == 0) {
+                cout << "- Front of Page - " << "\n\n";
+            } else {
+                i--;
+                printStudentList(vector<Student>(list.begin() + i*5, list.begin() + i*5+5));
+            }
+        } else { // input == 3
+            break;
+        }
+        while(true){
+            cout << "1. Next Page\n"
+                    "2. Previous Page\n"
+                    "3. Back to Menu\n"
+                    "\nEnter number : ";
+            cin >> input;
+            if(input == 1 || input == 2 || input == 3){
+                break;
+            } else {
+                cout << "Wrong input. Try again.\n";
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     StudentListManager *stu = new StudentListManager("data.txt");
 
     while (true) {
         string selection;
-        cout
-                << "1. Add Student\n2. Edit Student\n3. Show All\n4. Search Student\n5. Kick Student\n0. Exit\n\nEnter number : ";
+
+        cout << "1. Add Student\n"
+                "2. Edit Student\n"
+                "3. Show All\n"
+                "4. Search Student\n"
+                "5. Kick Student\n"
+                "0. Exit\n"
+                "\nEnter Menu number : ";
+
         cin >> selection;
+
         if (selection == "0") {
             cout << "Saved.";
             break;
+
         } else if (selection == "1") {
             string name, age, id, dept, tel;
             cout << "Enter Name : ";
@@ -38,8 +88,8 @@ int main(int argc, char *argv[]) {
             cin >> dept;
             cout << "Enter Telephone : ";
             cin >> tel;
-
             stu->insert(*(new Student(name, age, id, dept, tel)));
+
         } else if (selection == "2") {
             string name, age, id, dept, tel;
             cout << "Enter ID : ";
@@ -55,49 +105,53 @@ int main(int argc, char *argv[]) {
                 cin >> tel;
                 stu->modifyStudentInfo(id, name, dept, tel);
             }
+
         } else if (selection == "3") {
-            auto list = stu->getStudentList();
+            vector<Student> list = stu->getStudentList();
             cout << endl << "TOTAL MEMBER: " << list.size() << endl;
             if (list.size() < 5) {
                 printStudentList(list);
             }
             else{
-                int pageCount = list.size()/5, lastPageIndexes = list.size()%5;
-                for(int i=0;i<pageCount;i++){
-                    int input;
-                    printStudentList(vector<Student>(list.begin() + i*5, list.begin() + i*5+5));
-                    while(true){
-                        cout << "1. Next Page\n2. Break\n\nEnter number : ";
-                        cin >> input;
-                        if(input == 1){
-                            break;
-                        } else if(input == 2){
-                            break;
-                        } else {
-                            cout << "Wrong input. Try again.\n";
-                        }
-                    }
-                }
-                printStudentList(vector<Student>(list.begin() + pageCount*5, list.begin() + list.size()));
+                viewPagerStudentList(list);
             }
+
         } else if (selection == "4") {
             string mode, input;
-            vector<Student> k;
-            cout << "Search for...?\n1. ID\n2. Name\n3. Age\n4. Department\n";
-            cin >> input;
+            SearchMenu menu;
+            vector<Student> result;
+            cout << "Search for...?\n"
+                    "1. ID\n"
+                    "2. Name\n"
+                    "3. Age\n"
+                    "4. Department\n";
+            cout << "Enter Mode : ";
+            cin >> mode;
             while (true) {
                 //todo : ViewPager
-                if (input == "1") {
-                    k = stu->searching(input, Id);
-                } else if (input == "2") {
-                    k = stu->searching(input, Name);
-                } else if (input == "3") {
-                    k = stu->searching(input, Age);
-                } else if (input == "4") {
-                    k = stu->searching(input, Department);
+                if (mode == "1") {
+                    cout << "Enter ID : ";
+                    menu = Id;
+                } else if (mode == "2") {
+                    cout << "Enter Name : ";
+                    menu = Name;
+                } else if (mode == "3") {
+                    cout << "Enter Age : ";
+                    menu = Age;
+                } else if (mode == "4") {
+                    cout << "Enter Department : ";
+                    menu = Department;
                 } else {
                     cout << "Wrong input. Try again.\n";
                     continue;
+                }
+                cin >> input;
+                result = stu->searching(input, menu);
+
+                if(result.size() > 5) {
+                    viewPagerStudentList(stu->searching(input, menu));
+                } else {
+                    printStudentList(result);
                 }
                 break;
             }
@@ -108,13 +162,16 @@ int main(int argc, char *argv[]) {
             cin >> input;
             stu->deleting(input);
             cout << "Goodbye, student.";
+
         } else if (selection == "thanos") {
             stu->thanosFingerSnap();
             //todo : ascii art
             cout << "I'm inevitable." << endl;
+
         } else {
             cout << "Wrong input. Try again.\n";
         }
+
         cout << "\n";
 //        system("CLS");
     }
